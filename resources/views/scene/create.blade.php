768835fd5efb614f5413790b4726df72cc3059b9
Isaac Steiner <izakizak@gmail.com>
2018-06-14 21:33:31 -0400

    Added some headers

--- resources/views/scene/create.blade.php
+++ resources/views/scene/create.blade.php
@@ -14,18 +14,32 @@ Make a scene
         <div class="row">
             <div class="col-6">
                 <?php
+                    echo Form::label('playwright', 'Playwright');
                     echo Form::select('playwright', App\Playwright::all()->pluck('name','id'), null, ['placeholder' => 'Select a playwright...']);
                 ?>
             </div>
                 
             <div class="col-6">
                 <?php
+                    echo Form::label('play', 'Play');
                     echo Form::select('play', App\Play::all()->pluck('title','id'), null, ['placeholder' => 'Select a play...']);
                 ?>
             </div>
         </div>
     {{ Form::close() }}
-
+    <div class="row">
+        <div class="col">
+            <h2>Characters</h2>
+        </div>
+    </div>
+    <div class="row" id="character-list-headers">
+        <div class="col">
+            <h3>Name</h3>
+        </div>
+        <div class="col">
+            <h3>Description</h3>
+        </div>
+    </div>
     <div class="row" id="characters">
     </div>
 @endsection
